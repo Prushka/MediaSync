d1b9ae681f142d325b586707b8c083552abf07b4
Dan <dan@muddy.ca>
2021-10-10 00:21:54 -0400

    Update tvshow matching pattern

--- playground.py
+++ playground.py
@@ -1,3 +1,8 @@
+import re
 
 if __name__ == '__main__':
-    print("test")
+    a = "s0e3"
+    b = "abnc"
+    pattern = re.compile("^(s\d+e\d+)$")
+    print(bool(pattern.match("s190e22")))
+    print(re.findall(r'\d+', 's190e22'))
--- plex_control.py
+++ plex_control.py
@@ -12,14 +12,8 @@ r = redis.Redis(host='cloud.muddy.ca', port=6399, db=0, password="vWw@U4mzCw2am0
 lock = False
 EVERY = 0.1
 UPDATE_INTERVAL = 3600
-previous_print = ""
 
-
-def eprint(text):
-    global previous_print
-    # print(*args, file=sys.stderr, **kwargs)
-    print(str(text))
-    previous_print = str(text)
+sxex = re.compile("^(s\d+e\d+)$")
 
 
 def similar(a, b):
@@ -64,7 +58,7 @@ class KodiControlledPlayer:
         self.kodi = Kodi(self.kc)
         await self.kodi.ping()
         properties = await self.kodi.get_application_properties(["name", "version"])
-        eprint(properties)
+        print(properties)
         await self.update()
 
     async def waiting_signals(self):
@@ -81,17 +75,30 @@ class KodiControlledPlayer:
                 self.counter = 0
                 await self.update()
 
-    async def handler(self, message):
+    def find_id(self, content, id_name, lf_title, append_print):
+        print(f"Looking for: {lf_title}")
         sim = 0
-        id = 0
+        cid = 0
         title = ""
+        for c in content:
+            t_sim = similar(c['label'], lf_title)
+            if t_sim > sim:
+                sim = t_sim
+                cid = int(c[id_name])
+                title = c['label']
+        print(f"Found, highest matching score: {title}, {sim} {append_print}")
+        return cid
 
+    def is_season_episode(self, command):
+        return bool(sxex.match(command.lower()))
+
+    async def handler(self, message):
         data = message['data']
         if isinstance(data, int):
             return
         data = str(data.decode('utf-8'))
         args = data.split(" ")
-        eprint(args)
+        print(args)
         try:
             if len(args) == 0:
                 return
@@ -103,46 +110,42 @@ class KodiControlledPlayer:
                 if len(args) == 2 and not lock:
                     await self.kodi.media_seek(int(args[1]))
             elif args[0] == "movie":
-                g_title = data.replace(f"{args[0]} ", "")
-                eprint(f"Looking for: {g_title}")
-                for movie in self.movies["movies"]:
-                    t_sim = similar(movie['label'], g_title)
-                    if t_sim > sim:
-                        sim = t_sim
-                        id = int(movie['movieid'])
-                        title = movie['label']
-                eprint(f"Found movie with highest matching score: {title}, {sim}")
-                await self.kodi.play_item({"movieid": id})
+                if len(args) == 1:
+                    return
+                mid = self.find_id(self.movies["movies"], "movieid", data.replace(f"{args[0]} ", ""), "")
+                await self.kodi.play_item({"movieid": mid})
                 await engage_lock()
                 await self.kodi.media_seek(0)
-            elif args[0] == "show":
-                if len(args) < 3:
+            elif args[0] == "show" or self.is_season_episode(args[0]):
+                if len(args) < 2:
                     return
-                g_title = data.replace(f"{args[0]} {args[1]} {args[2]} ", "")
-                for show in self.tvshows["tvshows"]:
-                    t_sim = similar(show['label'], g_title)
-                    if t_sim > sim:
-                        sim = t_sim
-                        id = int(show['tvshowid'])
-                        title = show['label']
-                eprint(
-                    f"Found tvshow with highest matching score: {title}, {sim}, will navigate to season {args[1]} episode {args[2]}")
-                episodes = await self.kodi.get_episodes(id, int(args[1]))
+                if self.is_season_episode(args[0]):
+                    m = re.findall(r'\d+', args[0])
+                    lf_season = m[0]
+                    lf_episode = m[1]
+                    lf_title = data.replace(f"{args[0]} ", "")
+                else:
+                    lf_season = args[1]
+                    lf_episode = args[2]
+                    lf_title = data.replace(f"{args[0]} {args[1]} {args[2]} ", "")
+                tid = self.find_id(self.tvshows["tvshows"], "tvshowid", lf_title,
+                                   f"| will navigate to season {lf_season} episode {lf_episode}")
+                episodes = await self.kodi.get_episodes(tid, int(lf_season))
                 for episode in episodes['episodes']:
-                    if str(args[2]) in episode['label'][1:5]:
+                    if str(lf_episode) in episode['label'][1:5]:
                         await self.kodi.play_item({"episodeid": int(episode['episodeid'])})
                         await engage_lock()
                         await self.kodi.media_seek(0)
-                        break
+                        return
 
         except Exception:
-            eprint(traceback.format_exc())
+            print(traceback.format_exc())
 
 
 def inputs():
     global do_exit
     while True:
-        x = input(">")
+        x = input("")
         if x.lower() == "exit":
             do_exit = True
             return
@@ -151,12 +154,12 @@ def inputs():
 
 
 def print_help():
-    eprint("""Commands:
+    print("""Commands:
     play
     pause
     seek seconds
     movie movie_title
-    show season episode show_title
+    s3e2 show_title / show season episode show_title
     exit
     Note:
     PLEASE EXIT USING exit command
